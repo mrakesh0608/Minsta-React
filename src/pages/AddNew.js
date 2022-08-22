35be7998d88093ef5a68a463b83141d688693419
mrakesh0608 <mrakesh0608@gmail.com>
2022-08-22 10:24:36 +0530

    FFF

--- src/pages/AddNew.js
+++ src/pages/AddNew.js
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { useAuthContext } from 'hooks/useAuthContext'
 
