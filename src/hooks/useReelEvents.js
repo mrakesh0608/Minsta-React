cf6356b6633afd077634bdceeb8895ce2d419321
mrakesh0608 <mrakesh0608@gmail.com>
2022-09-17 10:27:46 +0530

    Reel

--- src/hooks/useReelEvents.js
+++ src/hooks/useReelEvents.js
@@ -3,112 +3,114 @@ import { iconPath } from 'helpers/Path';
 import { useAuthContext } from 'hooks/useAuthContext';
 import useFetch from 'hooks/useFetch';
 
-const usePostEvents = ({ setReel }) => {
+const useReelEvents = ({ setReel }) => {
     const { user } = useAuthContext();
 
     const { fetchData, isError } = useFetch();
     const navigate = useNavigate();
     //Like Start
-    const handleLikes = (e, post) => {
-        if (post.iLiked) {
+    const handleLikes = (e, reel) => {
+        if (reel.iLiked) {
             // ani_Like(e, false);
-            post.likes = post.likes - 1;
+            reel.likes = reel.likes - 1;
         }
         else {
             // ani_Like(e, true);
-            post.likes = post.likes + 1;
+            reel.likes = reel.likes + 1;
         }
-        post.iLiked = !post.iLiked;
-        // console.log(post);
-        setReel({ ...post });
-        update_Post_In_Server({
-            "_id": post._id,
+        reel.iLiked = !reel.iLiked;
+        // console.log(reel);
+        setReel({ ...reel });
+        update_reel_In_Server({
+            "_id": reel._id,
             "updateAttr": "liked_users",
             "username": user.Username,
-            "updateAdd": post.iLiked
+            "updateAdd": reel.iLiked
         });
     }
 
     //Like Animation Start
     const ani_Like = (e, flag) => {
 
-        const post = e.target.closest('.post');
+        const reel = e.target.closest('.reel');
         //remove all animated gifs if exists
-        clear_ani_on_post(post);
+        clear_ani_on_reel(reel);
 
         let time;
         if (flag) {
-            post.querySelector('.animation-like').innerHTML = '<img src="' + iconPath + 'ani_liked.gif" alt="like heart" class="ani-like">';
+            reel.querySelector('.animation-like').innerHTML = '<img src="' + iconPath + 'ani_liked.gif" alt="like heart" class="ani-like">';
             time = 850;
         }
         else {
-            post.querySelector('.animation-like').innerHTML = '<img src="' + iconPath + 'ani_unliked.gif" alt="like heart" class="ani-like">'
+            reel.querySelector('.animation-like').innerHTML = '<img src="' + iconPath + 'ani_unliked.gif" alt="like heart" class="ani-like">'
             time = 800;
         }
 
         //Remove all animated gifs after specific time
-        setTimeout(() => { clear_ani_on_post(post) }, time);
+        setTimeout(() => { clear_ani_on_reel(reel) }, time);
     }
-    function clear_ani_on_post(post) {
-        const repeat_ani = post.querySelectorAll('.ani-like');
-        Array.from(repeat_ani).forEach(post => {
-            post.remove();
+    function clear_ani_on_reel(reel) {
+        const repeat_ani = reel.querySelectorAll('.ani-like');
+        Array.from(repeat_ani).forEach(reel => {
+            reel.remove();
         });
     }
     //Like Animation End
     //Like End
 
     //Share - Start
-    const handleShare = async (e, post) => {
+    const handleShare = async (e, reel) => {
         try {
-            const post = e.target.closest('.reel');
-            const postContent = post.querySelector('video');
+            const src = e.target.closest('.reel').querySelector('video').src;
 
-            const response = await fetch(postContent.src);
+            const response = await fetch(src);
             const blob = await response.blob();
-
+            console.log(response);
             const filesArray = [
                 new File(
                     [blob],
-                    'image.jpg',
+                    'reel.mp4',
                     {
-                        type: "image/jpeg",
+                        type: blob.type,
                         lastModified: new Date().getTime()
                     }
                 )
             ];
-            const shareData = { files: filesArray };
-
+            console.log(reel);
+            const shareData = {
+                files: filesArray,
+                title: `Reel From ${reel.UserName}`,
+                text : reel.quote,
+                url:'https://minsta-react.herokuapp.com/reel'
+            };
             await navigator.share(shareData);
-            alert('post shared Successfully');
+            alert('reel shared Successfully');
         }
-        catch (err) {
-            alert('Error Occured\n\n' + err);
-        }
+        catch (err) {alert('Error Occured\n\n' + err);}
     }
     //Share -End
 
-    //Save Post - Start
-    const handleSave = (e, post) => {
-        post.iSaved = !post.iSaved;
-        setReel({ ...post });
-        update_Post_In_Server({
-            "_id": post._id,
+    //Save reel - Start
+    const handleSave = (e, reel) => {
+        reel.iSaved = !reel.iSaved;
+        setReel({ ...reel });
+        update_reel_In_Server({
+            "_id": reel._id,
             "updateAttr": "saved_users",
             "username": user.Username,
-            "updateAdd": post.iSaved
+            "updateAdd": reel.iSaved
         });
     }
-    //Save Post -End
-    const update_Post_In_Server = (payload) => {
+    //Save reel -End
+    const update_reel_In_Server = (payload) => {
 
         // fetchData({
-        //     path: '/posts/' + payload._id,
+        //     path: '/reel/' + payload._id,
         //     method: 'PATCH',
         //     payload
         // }).then(res => {
         //     if (res) {
-        //         console.log('post updated')
+        //         console.log('reel updated')
         //         setReel(res);
         //     }
         //     else console.log(res);
@@ -117,4 +119,4 @@ const usePostEvents = ({ setReel }) => {
 
     return { handleLikes, handleShare, handleSave, isError };
 }
-export default usePostEvents;
+export default useReelEvents;
