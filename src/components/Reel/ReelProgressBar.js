c33874219be90f5ce56d3f5e7cebe4dee0190f20
mrakesh0608 <mrakesh0608@gmail.com>
2022-09-19 18:15:56 +0530

    Version 1

--- src/components/Reel/ReelProgressBar.js
+++ src/components/Reel/ReelProgressBar.js
@@ -6,11 +6,11 @@ const ReelProgressBar = ({ currentPlayingVideo }) => {
             const val = (e.target.currentTime / e.target.duration) * document.getElementById('bar').offsetWidth;
             document.documentElement.style.setProperty('--progress', `${val}px`);
         })
-        // return function exit(){
-        //     reel?.removeEventListener('timeupdate',(e)=>{
-        //         console.log(e.target.currentTime,e.target.duration,document.getElementById('bar').offsetWidth);
-        //     })
-        // }
+        return function exit(){
+            currentPlayingVideo?.removeEventListener('timeupdate',(e)=>{
+                console.log(e.target.currentTime,e.target.duration,document.getElementById('bar').offsetWidth);
+            })
+        }
     }, [currentPlayingVideo]);
 
     const handleSeek = (e)=>{
