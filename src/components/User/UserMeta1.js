7931c4a7fbf1af086ad26210de3d5ecbf32799e3
mrakesh0608 <mrakesh0608@gmail.com>
2022-09-20 17:58:29 +0530

    VERSION 1.2

--- src/components/User/UserMeta1.js
+++ src/components/User/UserMeta1.js
@@ -10,7 +10,7 @@ const UserMeta1 = ({ path, user: userData }) => {
         initialize();
     }, [])
     //prop update to reflect
-    useEffect(()=>setData(userData),[userData]);
+    useEffect(() => setData(userData), [userData]);
 
     const initialize = () => {
         if (path) fetchData({ path, method: "GET" })
@@ -18,37 +18,38 @@ const UserMeta1 = ({ path, user: userData }) => {
     }
     return (
         <>
-            {isError ?
-                (isError === 'No Such User' ?
-                    navigate('/notfound/No Such User') :
-                    <div className='error'>{isError}</div>
-                ) :
-                (isPending && <div className="load">Loading ...</div>)
-            }
+            {isError && (isError === 'No Such User' ?
+                navigate('/notfound/No Such User') :
+                <div className='error'>{isError}</div>
+            )}
             <div className='user-meta-1'>
                 <div className="user-meta-1-1">
                     <div className='userimg'>
                         <img src={userIcon} alt="user" className='icons' />
                     </div>
                     <div className='network'>
-                        <a href='#user-posts-tags'>
-                            <div>
-                                <div className='network-count'>{user ? user.Posts : '-'}</div>
-                                <div>Posts</div>
-                            </div>
-                        </a>
-                        <Link to={'/user/' + user?.Username + '/followers'}>
-                            <div>
-                                <div className='network-count'>{user ? user.Followers : '-'}</div>
-                                <div>Followers</div>
-                            </div>
-                        </Link>
-                        <Link to={'/user/' + user?.Username + '/followings'}>
-                            <div>
-                                <div className='network-count'>{user ? user.Following : '-'}</div>
-                                <div>Following</div>
-                            </div>
-                        </Link >
+                        {isPending ? <div className="load">Loading ...</div> :
+                            <>
+                                <a href='#user-posts-tags'>
+                                    <div>
+                                        <div className='network-count'>{user ? user.Posts : '-'}</div>
+                                        <div>Posts</div>
+                                    </div>
+                                </a>
+                                <Link to={`/user/${user?.Username}/followers`}>
+                                    <div>
+                                        <div className='network-count'>{user ? user.Followers : '-'}</div>
+                                        <div>Followers</div>
+                                    </div>
+                                </Link>
+                                <Link to={`/user/${user?.Username}/followings`}>
+                                    <div>
+                                        <div className='network-count'>{user ? user.Following : '-'}</div>
+                                        <div>Following</div>
+                                    </div>
+                                </Link >
+                            </>
+                        }
                     </div>
                 </div>
                 <div className='user-meta-1-2'>
