31d45c690fe5f401ebf6d91d5d5b5767b9c16803
Merugu Rakesh <101246871+mrakesh0608@users.noreply.github.com>
2022-12-08 18:29:32 +0530

    Bye Bye Heroku

--- src/helpers/Path.js
+++ src/helpers/Path.js
@@ -2,7 +2,7 @@ const originPath = window.location.origin;
 const iconPath = originPath + '/icons/';
 const postPath = originPath + '/posts/';
 
-const url = 'https://minsta.onrender.com';   //https://minsta-server.herokuapp.com';
+const url = 'https://minsta.onrender.com';  //https://minsta-server.herokuapp.com';
 // const url = 'http://localhost:8000';
 
 export { originPath, iconPath, postPath,url};
